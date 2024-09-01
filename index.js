8fe3a8c997e533a41446ce04b22f979da1d7825e
jonas-ar <jonasfon@alu.ufc.br>
2024-08-31 23:48:07 -0300

    estrutura inicial de pastas

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// design
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// rotas
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// escuta do servidor
+app.listen(PORT, () => {
+  console.log(`The app start on http://localhost:${PORT}`);
+});
