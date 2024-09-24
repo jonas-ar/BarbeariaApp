3256476468587ded0ad565e552b1774d5933e371
Raniery Alves Vasconcelos <58216614+RanieryAV@users.noreply.github.com>
2024-09-24 17:24:03 -0300

    feat: back end da barra de busca para "Meus agendamentos"
    - Mudança de alguns nomes de botão para adicionar clareza ao site

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ const middleware = require("./utils/middleware");
 const indexRouter = require("./routes/index");
 const agendamentoRouter = require("./routes/agendamento");
 const meusagendamentosRouter = require("./routes/meusagendamentos");
+const buscarAgendamentosRouter = require("./routes/buscarAgendamentos")
 
 // db connection
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://admin:admin@cluster0.ltclo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  //"mongodb+srv://admin:admin@cluster0.ltclo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb://localhost:27017/barbearia_database"
 );
 
 // middleware
 middleware(app);
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use("/", indexRouter);
 app.use("/agendamento", agendamentoRouter);
 app.use("/meusagendamentos", meusagendamentosRouter);
+app.use("/buscarAgendamentos", buscarAgendamentosRouter)
 
 // server listen
 app.listen(PORT, () => {
